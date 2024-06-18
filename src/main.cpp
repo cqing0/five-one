95a5e2fc42abda548e90cc19c163a0acbbc78585
cqing0 <boerbasher@protonmail.com>
2024-06-18 17:16:32 +0100

    Create output loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,58 @@
 #include <windows.h>
+#include "glew.h"
 #include <GL/gl.h>
 #include "SDL.h"
 
 #define SDL_main main
 
+// TODO: Rescope these globals?
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
+void Init_SDL_GL()
+{
+    // Set OpenGL profiles
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    // Set OpenGL version
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    // RGBA: 8 bit
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    // Doublebuffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    // Hardware acceleration
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+}
+
 int main(int argc, char** argv)
 {
+	Init_SDL_GL();
+
 	window = SDL_CreateWindow("Game", 50, 50, 600, 600, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(5000);
+
+	bool quit = false;
+	SDL_Event event;
+
+	// Main loop
+	while (!quit) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) quit = true;
+		}
+		// Generate output
+		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+	// Free necessary data on shutdown
+	window = NULL;
+	renderer = NULL;
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
 	return 0;
 }
