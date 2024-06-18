c14f7e0d108271f5fddb6c3e61c8edb8daf8ab2e
cqing0 <boerbasher@protonmail.com>
2024-06-18 16:52:53 +0100

    Create magenta renderer

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 main: src/main.cpp
-	cl /Zi src/main.cpp /I"src\\include" /Fe"build\\five-one.exe" /Fo"build\\" /link /LIBPATH:"src\\lib"
+	cl /Zi src/main.cpp /I"src\\include" /Fe"build\\five-one.exe" /Fo"build\\" /link /LIBPATH:"src\\lib" opengl32.lib SDL2main.lib SDL2.lib
 	
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
-int main()
+#include <windows.h>
+#include <GL/gl.h>
+#include "SDL.h"
+
+#define SDL_main main
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+int main(int argc, char** argv)
 {
+	window = SDL_CreateWindow("Game", 50, 50, 600, 600, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(5000);
+	SDL_Quit();
 	return 0;
 }
