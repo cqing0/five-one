6b1d8aea0741f538360bc884d231e68de36e376a
cqing0 <boerbasher@protonmail.com>
2024-06-18 20:13:42 +0100

    Create OpenGL backbuffer

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include "glew.h"
 #include <GL/gl.h>
 #include "SDL.h"
+#include <stdio.h>
 
 #define SDL_main main
 
 // TODO: Rescope these globals?
 SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+SDL_GLContext context = NULL;
+
+// OpenGL vert data
+GLuint vertex_array_id;
 
 void Init_SDL_GL()
 {
@@ -27,12 +31,34 @@ void Init_SDL_GL()
     SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 }
 
+void Init_GL_Objects()
+{
+	glGenVertexArrays(1, &vertex_array_id);
+	glBindVertexArray(vertex_array_id);
+}
+
 int main(int argc, char** argv)
 {
-	Init_SDL_GL();
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+	}
 
-	window = SDL_CreateWindow("Game", 50, 50, 600, 600, SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, 0);
+	Init_SDL_GL();
+	/* Init_GL_Objects(); */
+
+	window = SDL_CreateWindow("Game", 50, 50, 600, 600, SDL_WINDOW_OPENGL);
+
+    context = SDL_GL_CreateContext(window);
+	if (context == NULL) {
+        printf("error creating context: %s\n", SDL_GetError());
+	}
+
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        printf("error: %d\n", glGetError());
+    }
 
 	bool quit = false;
 	SDL_Event event;
@@ -43,16 +69,14 @@ int main(int argc, char** argv)
 			if (event.type == SDL_QUIT) quit = true;
 		}
 		// Generate output
-		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT );
+		SDL_GL_SwapWindow(window);
 	}
 
 	// Free necessary data on shutdown
 	window = NULL;
-	renderer = NULL;
 	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
 	return 0;
 }
